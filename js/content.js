eb8e853894907e704eb64e335c861aed8c1ca6c4
thuantruong <hi.tim.gi.the@gmail.com>
2024-03-04 14:53:14 +0700

    responsive app

--- js/content.js
+++ js/content.js
@@ -11,6 +11,9 @@ let newMentor = [...mentors];
 
 let limitCourse = 6;
 let limitMentor = 3;
+if (window.innerWidth < 768) {
+  limitMentor = 5;
+}
 let mode;
 let valueCourse = '';
 let valueMentor = '';
@@ -27,7 +30,7 @@ const contentNavlink = () => {
   navLinkEl.innerHTML = "";
   let content = "";
   navLinks.forEach((nav, index) => {
-    content += `<div onclick="onClick(event, ${index})" class="text-xl text-[#535A65] font-semibold cursor-pointer ${
+    content += `<div onclick="onClick(event, ${index})" class="text-md md:text-xl text-[#535A65] font-semibold cursor-pointer ${
       index === 0 ? "active--link" : ""
     } navbar-link">${nav}</div>`;
   });
@@ -115,41 +118,41 @@ const contentRegister = () => {
 const bookLearn = () => {
   contentEl.innerHTML = "";
   contentEl.innerHTML = `
-    <div class="w-5/6 p-6 rounded-lg bg-[#FAFAFA]">
+    <div class="w-full md:w-5/6 p-4 md:p-6 rounded-lg bg-[#FAFAFA]">
       <form class="flex flex-col gap-4" onsubmit='onSearch(event)'>
         <div class="italic">Học viên có thể đăng ký học thử, đặt lịch trực tiếp với gia sư theo ý thích. </div>
         <div class="flex justify-between items-center">
-          <label class="font-semibold flex-auto">Chọn môn học <span class="text-[red]">*</span></label>
-          <div class="bg-white py-2 px-4 rounded-md flex" onclick='onSelectCourse(event)'>
-            <input class="w-[300px] bg-white" disabled value='${valueCourse}' type="text" name="course" placeholder="Danh sách môn học" />
-            <button class="bg-primary py-1 px-2 rounded text-white hover:opacity-70">Chọn</button>
+          <label class="whitespace-nowrap font-semibold flex-auto text-sm md:text-base">Chọn môn học <span class="text-[red]">*</span></label>
+          <div class="bg-white py-1 md:py-2 px-2 md:px-4 rounded-md flex" onclick='onSelectCourse(event)'>
+            <input class="w-[130px] md:w-[300px] text-sm md:text-base bg-white text-ellipsis" disabled value='${valueCourse}' type="text" name="course" placeholder="Danh sách môn học" />
+            <button class="bg-primary py-1 px-2 text-sm md:text-base rounded text-white hover:opacity-70">Chọn</button>
           </div>
         </div>
-        <div class="flex justify-between items-center">
-          <label class="font-semibold">Chọn ngày</label>
+        <div class="flex flex-col md:flex-row justify-between items-start md:items-center">
+          <label class="font-semibold text-sm md:text-base whitespace-nowrap">Chọn ngày</label>
           <div class="flex items-center gap-1">
-            <span>Từ</span>
-            <div class="flex items-center p-2 rounded-md border w-36 bg-white mr-1">
-              <input id="flatpickr-start" class="outline-none w-28" onfocus="onSelectedDateStart(event)" type="text">
-              <i class="fa-regular fa-calendar"></i>
+            <span class="text-sm md:text-base">Từ</span>
+            <div class="flex items-center p-1 md:p-2 rounded-md border w-28 md:w-36 bg-white mr-1">
+              <input class="outline-none w-24 text-sm md:text-base md:w-28" onfocus="onSelectedDateStart(event)" type="text">
+              <i class="hidden md:block fa-regular fa-calendar"></i>
             </div>
-            <span>Đến ngày</span>
-            <div class="flex items-center p-2 rounded-md border w-36 bg-white">
-              <input class="w-28 outline-none" onfocus="onSelectedDateEnd(event)" type="text">
-              <i class="fa-regular fa-calendar"></i>
+            <span class="text-sm md:text-base whitespace-nowrap">Đến ngày</span>
+            <div class="flex items-center p-2 rounded-md border w-28 md:w-36 bg-white">
+              <input class="w-24 md:w-28 text-sm md:text-base outline-none" onfocus="onSelectedDateEnd(event)" type="text">
+              <i class="hidden md:block fa-regular fa-calendar"></i>
             </div>
           </div>
         </div>
         <div class="flex justify-between items-center">
-          <label class="font-semibold flex-auto">Chọn gia sư</label>
-          <div class="bg-white py-2 px-4 rounded-md flex" onclick='onSelectMentor(event)'>
-            <input class="w-[300px] disabled bg-white" disabled type="text" name="mentor" value='${valueMentor}' placeholder="Chọn gia sư" />
-            <button class="bg-primary py-1 px-2 rounded text-white hover:opacity-70">Chọn</button>
+          <label class="font-semibold text-sm md:text-base flex-auto whitespace-nowrap">Chọn gia sư</label>
+          <div class="bg-white py-1 md:py-2 px-2 md:px-4 rounded-md flex" onclick='onSelectMentor(event)'>
+            <input class="w-[130px] md:w-[300px] text-sm md:text-base disabled bg-white text-ellipsis" disabled type="text" name="mentor" value='${valueMentor}' placeholder="Chọn gia sư" />
+            <button class="bg-primary py-1 px-2 rounded text-sm md:text-base text-white hover:opacity-70">Chọn</button>
           </div>
         </div>
         <div class="flex justify-between items-center">
-          <div><span class="text-[red]">*</span> Phần không được bỏ trống</div>
-          <div><button class="text-white bg-primary py-2 px-12 hover:opacity-80 rounded-md">Tìm kiếm</button></div>
+          <div class="text-sm md:text-base"><span class="text-[red]">*</span> Phần không được bỏ trống</div>
+          <div><button class="text-white text-sm md:text-base bg-primary py-1.5 md:py-2 px-6 md:px-12 hover:opacity-80 rounded-md">Tìm kiếm</button></div>
         </div>
       </form>
     </div>
@@ -205,8 +208,8 @@ const scheduleLearn = () => {
   contentEl.innerHTML = `
     <div>
       <div>
-        <h2 class="text-3xl">Buổi học sắp diễn ra</h2>
-        <div class="mt-4 mb-6">
+        <h2 class="text-xl md:text-3xl font-semibold">Buổi học sắp diễn ra</h2>
+        <div class="mt-3 md:mt-4 mb-6">
           <div class="italic">Vui lòng truy cập đường link zoom để tham gia buổi học</div>
           <div class="italic">Lưu ý: Để buổi học đạt được hiệu quả, học viên cần chuẩn bị các nội dung sau:</div>
           <div class="italic">- Tham dự đúng giờ</div>
@@ -215,27 +218,27 @@ const scheduleLearn = () => {
         </div>
         <table class="w-full">
           <div class="h-[10px] rounded-t-md bg-primary"></div>
-          <tr class="bg-primary text-white rounded-t-xl">
-            <th class="px-4 pb-4 pt-3">Thời gian</th>
-            <th class="px-4 pb-4 pt-3">Gia sư</th>
-            <th class="px-4 pb-4 pt-3">Môn học</th>
-            <th class="px-4 pb-4 pt-3">Hình thức</th>
-            <th class="px-4 pb-4 pt-3">Zoom ID</th>
-            <th class="px-4 pb-4 pt-3"></th>
+          <tr class="bg-primary text-white">
+            <th class="px-0 md:px-4 pb-2 md:pb-4 pt-1 md:pt-3 text-xs md:text-base">Thời gian</th>
+            <th class="px-0 md:px-4 pb-2 md:pb-4 pt-1 md:pt-3 text-xs md:text-base">Gia sư</th>
+            <th class="px-0 md:px-4 pb-2 md:pb-4 pt-1 md:pt-3 text-xs md:text-base">Môn học</th>
+            <th class="px-0 md:px-4 pb-2 md:pb-4 pt-1 md:pt-3 text-xs md:text-base">Hình thức</th>
+            <th class="px-0 md:px-4 pb-2 md:pb-4 pt-1 md:pt-3 text-xs md:text-base">Zoom ID</th>
+            <th class="px-0 md:px-4 pb-2 md:pb-4 pt-1 md:pt-3 text-xs md:text-base"></th>
           </tr>
           ${scheduleList()}
         </table>
       </div>
       <div>
-        <h2 class="text-3xl mt-8 mb-4">Buổi học đã diễn ra/ Đã hủy</h2>
+        <h2 class="text-xl md:text-3xl font-semibold mt-8 mb-4">Buổi học đã diễn ra/ Đã hủy</h2>
         <table class="w-full">
-          <div class="h-[10px] rounded-t-md bg-primary"></div>
+          <div class="h-[9px] rounded-t-md bg-primary"></div>
           <tr class="bg-primary text-white rounded-t-xl">
-            <th class="px-4 pb-4 pt-3">Thời gian</th>
-            <th class="px-4 pb-4 pt-3">Gia sư</th>
-            <th class="px-4 pb-4 pt-3">Môn học</th>
-            <th class="px-4 pb-4 pt-3">Ghi chú</th>
-            <th class="px-4 pb-4 pt-3">Trạng thái</th>
+            <th class="px-0 md:px-4 pb-2 md:pb-4 pt-1 md:pt-3 text-xs md:text-base">Thời gian</th>
+            <th class="px-0 md:px-4 pb-2 md:pb-4 pt-1 md:pt-3 text-xs md:text-base">Gia sư</th>
+            <th class="px-0 md:px-4 pb-2 md:pb-4 pt-1 md:pt-3 text-xs md:text-base">Môn học</th>
+            <th class="px-0 md:px-4 pb-2 md:pb-4 pt-1 md:pt-3 text-xs md:text-base">Ghi chú</th>
+            <th class="px-0 md:px-4 pb-2 md:pb-4 pt-1 md:pt-3 text-xs md:text-base">Trạng thái</th>
           </tr>
           ${scheduleCancel()}
         </table>
@@ -248,12 +251,12 @@ const scheduleLearn = () => {
     for (const schedule of schedules) {
       listContent += `
         <tr class="even:bg-gray-100 text-center">
-          <td class="p-4">${schedule.time}</td>
-          <td class="p-4">${schedule.mentor}</td>
-          <td class="p-4">${schedule.course}</td>
-          <td class="p-4">${schedule.mode}</td>
-          <td class="p-4">${schedule.zoomId}</td>
-          <td class="p-4"><button class="text-[red] text-[20px] hover:opacity-70"><i class="fa-regular fa-circle-xmark"></i></button></td>
+          <td class="p-0.5 md:p-4 text-xs md:text-base">${schedule.time}</td>
+          <td class="p-0.5 md:p-4 text-xs md:text-base">${schedule.mentor}</td>
+          <td class="p-0.5 md:p-4 text-xs md:text-base">${schedule.course}</td>
+          <td class="p-0.5 md:p-4 text-xs md:text-base">${schedule.mode}</td>
+          <td class="p-0.5 md:p-4 text-xs md:text-base">${schedule.zoomId}</td>
+          <td class=""><button class="text-[red] p-0.5 md:p-4 text-sm md:text-[20px] hover:opacity-70"><i class="fa-regular fa-circle-xmark"></i></button></td>
         </tr>
       `;
     }
@@ -265,11 +268,11 @@ const scheduleLearn = () => {
     for (const schedule of schedules) {
       listContent += `
         <tr class="even:bg-gray-100 text-center">
-          <td class="p-4">${schedule.time}</td>
-          <td class="p-4">${schedule.mentor}</td>
-          <td class="p-4">${schedule.course}</td>
-          <td class="p-4">${schedule.mode}</td>
-          <td class="p-4">${schedule.zoomId}</td>
+          <td class="p-0.5 md:p-4 text-xs md:text-base">${schedule.time}</td>
+          <td class="p-0.5 md:p-4 text-xs md:text-base">${schedule.mentor}</td>
+          <td class="p-0.5 md:p-4 text-xs md:text-base">${schedule.course}</td>
+          <td class="p-0.5 md:p-4 text-xs md:text-base">${schedule.mode}</td>
+          <td class="p-0.5 md:p-4 text-xs md:text-base">${schedule.zoomId}</td>
         </tr>
       `;
     }
@@ -305,15 +308,15 @@ function listCourse(data) {
   let content = '';
   data.forEach(course => {
     content += `
-    <li class="flex items-center gap-4">
-      <div class="basic-1/5">
-        <img class="w-[150px]" src="${course.picture}" alt="${course.name}" />
+    <li class="flex justify-between items-center gap-4">
+      <div class="basic-1/5 grow-0 shrink-0 basis-auto">
+        <img class="w-[80px]" src="${course.picture}" alt="${course.name}" />
       </div>
       <div class="basic-3/5">
-        <h3 class="text-lg font-semibold">${course.name}</h3>
+        <h3 class="text-md md:text-lg font-semibold">${course.name}</h3>
         <p class="text-sm text-slate-800 w-[150px] text-ellipsis overflow-hidden h-[40px]">${course.description}</p>
       </div>
-      <div class="basic-1/5"><button onclick='onChooseCourse(${JSON.stringify(course)})' class="bg-primary text-white py-1 px-4 rounded">Chọn</button></div>
+      <div class="basic-1/5"><button onclick='onChooseCourse(${JSON.stringify(course)})' class="bg-primary text-sm md:text-base text-white py-1 px-4 rounded">Chọn</button></div>
     </li>
     `;
   })
@@ -324,16 +327,16 @@ function listMentor(data) {
   let content = '';
   data.forEach(mentor => {
     content += `
-    <li class="flex items-center justify-start w-fit gap-8 border border-slate-200 rounded-md">
-      <div class="w-[90px]">
+    <li class="flex items-center justify-start w-fit gap-4 md:gap-8 border border-slate-200 rounded-md">
+      <div class="w-[90px] grow-0 shrink-0 basis-auto">
         <img class="w-full rounded-md" src="${mentor.picture}" alt="${mentor.name}" />
       </div>
       <div class="">
-        <h3 class="text-lg font-semibold">${mentor.name}</h3>
+        <h3 class="text-md md:text-lg font-semibold">${mentor.name}</h3>
         <p class="text-sm text-slate-800 text-ellipsis overflow-hidden max-h-[40px]">${mentor.workPlace}</p>
         <span class="text-md font-semibold text-[#8D939E]">Mã gia sư: ${mentor.code}</span>
       </div>
-      <div class="basic-1/5 pr-4"><button onclick='onChooseMentor(${JSON.stringify(mentor)})' class="bg-primary text-white py-1 px-4 rounded">Chọn</button></div>
+      <div class="basic-1/5 pr-4"><button onclick='onChooseMentor(${JSON.stringify(mentor)})' class="bg-primary text-sm md:text-base text-white py-1 px-4 rounded">Chọn</button></div>
     </li>
     `;
   })
@@ -433,13 +436,13 @@ function createdPagination(totalPages, page) {
   if (mode === 'COURSE') {
 
     popupEl.innerHTML = `
-      <div class="w-6/12 h-5/6 relative py-10 px-12 mt-[60px] m-auto gap-6 bg-white rounded">
+      <div class="w-full md:w-6/12 h-screen md:h-5/6 relative py-4 md:py-10 px-4 md:px-12 mt-0 md:mt-[60px] m-auto gap-6 bg-white rounded">
         <div class="absolute right-4 top-2 text-2xl hover:opacity-60 cursor-pointer" onclick="onClose()"><i class="fa-solid fa-xmark"></i></div>
         <div>
-          <h2 class="text-2xl font-bold border-l-2 border-solid border-black pl-2">Chọn môn học</h2>
-          <div class="relative min-w-[200px] group my-6 py-1 px-4 border rounded-xl w-[200px]">
+          <h2 class="text-xl md:text-2xl font-bold border-l-2 border-solid border-black pl-2">Chọn môn học</h2>
+          <div class="relative min-w-[200px] group my-4 md:my-6 py-1 px-4 border rounded-xl w-full md:w-[200px]">
             <div class="py-1 px-2 rounded w-full flex items-center justify-between cursor-pointer bg-white gap-4">
-              <span class="text-black text-md font-semibold" id="showCourse">
+              <span class="text-black text-sm md:text-base font-semibold" id="showCourse">
                 - Lọc theo môn -
               </span>
               <svg xmlns="http://www.w3.org/2000/svg" width="10" height="6" viewBox="0 0 10 6" fill="none">
@@ -448,15 +451,15 @@ function createdPagination(totalPages, page) {
             </div>
             <div class="opacity-0 invisible group-hover:opacity-100 group-hover:visible rounded bg-white absolute top-[100%] left-0 right-0 z-50 max-h-[300px] shadow transition overflow-y-auto">
               ${filterCoursesList.map(item => {
-                return `<div onclick='onFilterCourse(${JSON.stringify(item)})' class="py-1 px-2 text-black cursor-pointer hover:bg-[#0417764d] text-black">${item.toString()}</div>`
+                return `<div onclick='onFilterCourse(${JSON.stringify(item)})' class="py-1 px-2 text-sm md:text-base text-black cursor-pointer hover:bg-[#0417764d] text-black">${item.toString()}</div>`
               }).join('')}
             </div>
           </div>
         </div>
-        <ul class="grid grid-cols-2 gap-8">
+        <ul class="grid grid-cols-1 md:grid-cols-2 gap-4 md:gap-6">
           ${listCourse(result)}
         </ul>
-        <div class="pagination mt-8">
+        <div class="pagination mt-4">
           <ul>
             ${liTag}
           </ul>
@@ -466,19 +469,19 @@ function createdPagination(totalPages, page) {
   }
   if (mode === 'MENTOR') {
     popupEl.innerHTML = `
-      <div class="relative w-6/12 h-5/6 relative py-6 px-12 mt-[60px] m-auto gap-6 bg-white rounded">
+      <div class="relative w-full md:w-6/12 h-screen md:h-5/6 py-4 md:py-6 px-4 md:px-12 mt-0 md:mt-[60px] m-auto gap-6 bg-white rounded">
         <div class="absolute right-4 top-2 text-2xl hover:opacity-60 cursor-pointer" onclick="onClose()"><i class="fa-solid fa-xmark"></i></div>
         <div>
-          <h2 class="text-2xl font-bold border-l-2 border-solid border-black pl-2">Chọn giáo viên gia sư</h2>
+          <h2 class="text-xl md:text-2xl font-bold border-l-2 border-solid border-black pl-2">Chọn giáo viên gia sư</h2>
           <div class="w-80 rounded-xl border py-2 px-2 my-3">
             <i class="fa-solid fa-magnifying-glass text-slate-400"></i>
-            <input oninput="onSearchMentor(event)" class="outline-none w-64 ml-2" type="text" placeholder="Tìm theo tên/mã gia sư" name="search" />
+            <input oninput="onSearchMentor(event)" class="text-sm md:text-base outline-none w-64 ml-2" type="text" placeholder="Tìm theo tên/mã gia sư" name="search" />
           </div>
         </div>
         <ul class="flex flex-col gap-4">
           ${listMentor(result)}
         </ul>
-        <div class="pagination mt-6">
+        <div class="pagination mt-4 md:mt-6">
           <ul>
             ${liTag}
           </ul>
